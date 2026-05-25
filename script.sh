c01a66a42c5b8d38edebc70bb4bd299975331893
gautamgithu <g10autam@gmail.com>
2026-05-25 12:12:10 +0530

    my first automation script ready

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 1. Teeno files banana aur unme text likhna
+echo "Task 1 complete ho gya" > file1.txt
+echo "Task 2 complete ho gya" > file2.txt
+echo "Task 3 complete ho gya" > file3.txt
+
+# 2. Strict permission lagana (Sirf AAP padh-likh sakte ho)
+chmod 700 file1.txt file2.txt file3.txt
